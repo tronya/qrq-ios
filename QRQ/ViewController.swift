e019ca18a2bbf15cc297e4bf4543a5a9f16d06da
Yura Tronyak <yuratronyak@yuras-mac.local>
2016-04-28 03:11:37 -0700

    Creating of app

--- QRQ/ViewController.swift
+++ QRQ/ViewController.swift
@@ -14,32 +14,35 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
 
     
 
-    
-    let locationManager = CLLocationManager()
-    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
     @IBOutlet weak var Geo_label: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+
     
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Ask for Authorisation from the User.
-        self.locationManager.requestAlwaysAuthorization()
-        
-        // For use in foreground
-        self.locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
         
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
             
-            let startLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 51.50998, longitude: +26.5838873007047)
-            let monitoredRegion = CLCircularRegion(center: startLocation, radius: 100, identifier: "Region Test")
-            locationManager.startMonitoringForRegion(monitoredRegion)
+            mapView.showsUserLocation = true
             
-            mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+            let dod_1 = CLCircularRegion(center: CLLocationCoordinate2D(latitude: 48.680757725957, longitude: 26.57858588604), radius: 20, identifier: "dod_1")
+            locationManager.startMonitoringForRegion(dod_1)
+
+            let dod_2 = CLCircularRegion(center: CLLocationCoordinate2D(latitude: 48.68104239, longitude: 26.58391804), radius: 20, identifier: "dod_2")
+            locationManager.startMonitoringForRegion(dod_2)
+            
+            let dod_3 = CLCircularRegion(center: CLLocationCoordinate2D(latitude:48.68109375, longitude: 26.58671021), radius: 20, identifier: "dod_3")
+            locationManager.startMonitoringForRegion(dod_3)
         }
     }
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -47,25 +50,26 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         print("locations = \(locValue.latitude) \(locValue.longitude)")
         Geo_label.text = "\(locValue.latitude) \(locValue.longitude)"
 
-
-
-        
         let center = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
-        let newCircle = MKCircle(centerCoordinate: center, radius: 200 as CLLocationDistance)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         
         
         
         mapView.setRegion(region, animated: true)
-        mapView.addOverlay(newCircle)
+
+    }
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        
+        let alertController = UIAlertController(title: "In Region", message: "Your in \(region.identifier)", preferredStyle: .Alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(defaultAction)
+        presentViewController(alertController, animated: true, completion: nil)
+        print("Entering region \(region.identifier)")
+        
     }
     func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
         print("Starting monitoring \(region.identifier)")
         Geo_label.text = "didStartMonitoringForRegion"
-    }
-    func locationManager(manager: CLLocationManager, didEnterRegion region:CLRegion) {
-        print("boo")
-        Geo_label.text = "didEnterRegion"
     }
     
     
